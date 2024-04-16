84e0e2cc12dc9c14a996ea0518f2f738738f06b2
khoadz <khoa.soft.e@gmail.com>
2024-04-16 23:57:47 +0700

    feat: finished api integration

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,12 +3,20 @@ import { Link } from "react-router-dom";
 import Logo from "../assets/icons/logo.svg?react";
 import Languages from "../assets/icons/languages.svg?react";
 import Search from "../assets/icons/search.svg?react";
+import authService from "../services/auth.service";
+import { useAuth } from "../contexts/AuthContext";
 
 interface NavBarProps {
   className?: string;
 }
 
 const NavBar: React.FC<NavBarProps> = ({ className }) => {
+  const { setAccessToken } = useAuth();
+
+  const handleLogout = async () => {
+    const token = await authService.logout();
+    setAccessToken(token!);
+  };
   return (
     <nav
       className={`bg-white-background flex flex-row justify-start items-center p-1  ${className}`}
@@ -28,6 +36,12 @@ const NavBar: React.FC<NavBarProps> = ({ className }) => {
           alt="Image"
         />
         <Languages />
+        <button
+          className="bg-transparent hover:bg-primary-color text-primary-color font-semibold hover:text-white py-2 px-4 border border-primary-color hover:border-transparent rounded"
+          onClick={handleLogout}
+        >
+          Log Out
+        </button>
       </div>
     </nav>
   );
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,34 +1,74 @@
 // src/services/auth.service.ts
-import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
-
+const API_URL = "http://localhost:8080/api/v1/auth/";
 class AuthService {
-  login(username: string, password: string) {
-    return axios
-      .post(API_URL + "signin", {
-        username,
-        password,
-      })
-      .then((response) => {
-        if (response.data.accessToken) {
-          localStorage.setItem("user", JSON.stringify(response.data));
-        }
-
-        return response.data;
+  async login(username: string, password: string) {
+    try {
+      const response = await fetch(API_URL + "authenticate", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email: username, password: password }),
       });
+      console.log(response);
+      if (!response.ok) {
+        throw new Error("Login failed");
+      }
+
+      const data = await response.json();
+      if (data) {
+        console.log("Login successful", data);
+        localStorage.setItem("access_token", JSON.stringify(data.access_token));
+        return localStorage.getItem("access_token");
+      }
+    } catch (error) {
+      console.error("Login failed", error);
+      throw error;
+    }
   }
 
-  logout() {
-    localStorage.removeItem("user");
+  async logout() {
+    try {
+      localStorage.removeItem("access_token");
+      return localStorage.getItem("access_token");
+    } catch (error) {
+      console.error("Logout failed:", error);
+      throw error;
+    }
   }
 
-  register(username: string, email: string, password: string) {
-    return axios.post(API_URL + "signup", {
-      username,
-      email,
-      password,
-    });
+  async register(
+    firstname: string,
+    lastname: string,
+    username: string,
+    password: string
+  ) {
+    try {
+      const response = await fetch(API_URL + "register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          firstname: firstname,
+          lastname: lastname,
+          email: username,
+          password: password,
+          role: "USER",
+        }),
+      });
+      console.log(response);
+      if (!response.ok) {
+        throw new Error("Registration failed");
+      }
+
+      const data = await response.json();
+      if (data) {
+        console.log("Registration successful", data);
+        localStorage.setItem("access_token", JSON.stringify(data.access_token));
+        return localStorage.getItem("access_token");
+      }
+    } catch (error) {
+      console.error("Registration failed:", error);
+      throw error;
+    }
   }
 }
 
